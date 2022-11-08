4810eaa5f0d086ae5ad21efce2d2ca23cd88b79c
jschneiderm98 <julio_schneider@hotmail.com>
2022-11-08 14:07:31 -0300

    alterar teste

--- lcd_hitachi_hd_44780/temp.sh
+++ lcd_hitachi_hd_44780/temp.sh
@@ -1,7 +1,7 @@
-MOD=lcd_device_driver/data0
-TEXTO1="Muito linda"
-TEXTO2="minha"
-TEXTO3="namorada"
+MOD=lcd_device_driver/data
+TEXTO1="Teste1"
+TEXTO2="Teste2"
+TEXTO3="Teste3"
 echo Escrevendo $TEXTO1 no LCD
 sudo echo -n $TEXTO1 > /dev/$MOD
 echo Lendo o texto atual do LCD...
