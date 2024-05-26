ea158b4753d29bbae12b9098985d5963b8c6b96d
jschneiderm98 <julio_schneider@hotmail.com>
2024-05-25 23:52:54 -0300

    leitura e escrita nos registradores da tag picc

--- rfid-rc522/src/rfid_rc522_device_driver.c
+++ rfid-rc522/src/rfid_rc522_device_driver.c
@@ -1,3 +1,4 @@
+#include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/spi/spi.h>
@@ -131,6 +132,57 @@ void clear_bits_in_reg(mfrc522_registers reg, uint8_t bits_to_set) {
     write_to_register(reg, reg_current_byte & (~bits_to_set));
 }
 
+int isCrcOperationDone(void) {
+    uint8_t divIrqRegByte = read_from_register(DivIrqReg);
+    return divIrqRegByte & 0x04;
+}
+
+rc522_status calculate_crc(uint8_t *data, size_t data_size, uint8_t *result) {
+    uint16_t i;
+    write_to_register(CommandReg, Idle);
+    clear_bits_in_reg(DivIrqReg, 0x04);
+    set_bits_in_reg(FIFOLevelReg, 0x80);
+
+    //write the data to FIFO Buffer
+    write_to_register_multiple(FIFODataReg, data, data_size);
+
+    write_to_register(CommandReg, CalcCRC);
+
+    i = 5000;
+    while (i>0) {
+        if(isCrcOperationDone()) {
+            write_to_register(CommandReg, Idle);
+            result[0] = read_from_register(CRCResultRegLSB);
+            result[1] = read_from_register(CRCResultRegMSB);
+            return RC522_OK;
+        }
+        i--;
+    }
+    write_to_register(CommandReg, Idle);
+    return RC522_TIMEOUT;
+}
+
+uint8_t is_crc_from_picc_valid(uint8_t *data, uint8_t data_size) {
+    uint8_t result[2];
+    uint8_t data_size_without_crc = data_size - 2;
+    uint8_t crc_position = data_size - 2;
+
+    calculate_crc(data, data_size_without_crc, result);
+    
+    set_bits_in_reg(FIFOLevelReg, 0x80);
+    if(data[crc_position] != result[0] || data[crc_position+1] != result[1]) return 0;
+    return 1;
+}
+
+uint8_t is_uid_valid_picc(uint8_t *uid) {
+    uint8_t check = 0;
+    for (size_t i = 0; i < 4; i++)
+    {
+        check = check ^ uid[i];
+    }
+    return check == uid[4];
+}
+
 void rc522_reset(void) {
     write_to_register(CommandReg, SoftReset);
 }
@@ -196,14 +248,373 @@ void rc522_self_test(void) {
     rc522_reset();
 }
 
+rc522_status send_command(uint8_t command, uint8_t *data, size_t data_size, uint8_t **response, uint8_t *response_size, uint8_t *response_size_bits, uint8_t should_check_crc) {
+    uint8_t irqEn = 0x00;
+    uint8_t waitIrq = 0x00;
+
+    //uint8_t tx_last_bits = valid_bits ? valid_bits : 0;
+    //uint8_t bit_framing = tx_last_bits;
+
+    if(command == MFAuthent) {
+        irqEn = 0x12;
+        waitIrq = 0x10;
+    }
+
+    if(command == Transceive) {
+        irqEn = 0x77;
+        waitIrq = 0x30;
+    }
+    write_to_register(CommandReg, Idle);
+    write_to_register(ComlEnReg, irqEn | 0x80);
+    clear_bits_in_reg(ComIrqReg, 0x80);
+    set_bits_in_reg(FIFOLevelReg, 0x80);
+
+    write_to_register_multiple(FIFODataReg, data, data_size);
+
+    write_to_register(CommandReg, command);
+
+    if(command == Transceive) {
+        set_bits_in_reg(BitFramingReg, 0b10000000);
+    }
+
+    int i = 2000;
+
+    while (1) {
+        uint8_t comIrqRegByte = read_from_register(ComIrqReg);
+        if (i-- < 1 || comIrqRegByte & waitIrq) break;
+    }
+
+    clear_bits_in_reg(BitFramingReg, 0b10000000);
+
+    if(i < 1) return RC522_TIMEOUT;
+
+    uint8_t errorRegValue = read_from_register(ErrorReg); // ErrorReg[7..0] bits are: WrErr TempErr reserved BufferOvfl CollErr CRCErr ParityErr ProtocolErr
+    if (errorRegValue & 0x13) 
+    {  // BufferOvfl ParityErr ProtocolEr
+        printk(KERN_ERR"Error detected, errorRegValue: %x\n", errorRegValue);
+        return RC522_ERR;
+    }
+    if(read_from_register(ComIrqReg) & irqEn & 0x01) return RC522_NOTAGERR;
+
+    *response_size = read_from_register(FIFOLevelReg);
+    uint8_t last_bits = read_from_register(ControlReg) & 0b00000111;
+    if(*response_size > 0) {
+        read_from_register_multiple(FIFODataReg, response, *response_size);
+        *response_size_bits = last_bits ? ((*response_size) - 1) * 8 + last_bits : (*response_size)*8;
+    }
+
+    if(!should_check_crc) return RC522_OK;
+
+    if (*response_size < 3 || !is_crc_from_picc_valid(*response, *response_size)) return RC522_FAILED_CRC_CHECK;
+     
+    return RC522_OK;
+}
+
+rc522_status req_a_picc(uint8_t **res, uint8_t *res_size, uint8_t *res_size_bits) {
+    uint8_t req_mode = PICC_REQIDL;
+    write_to_register(BitFramingReg, 0x07);
+
+    rc522_status status = send_command(Transceive, &req_mode, 1, res, res_size, res_size_bits, 0);
+
+    if(status != RC522_OK) return status;
+    if(*res_size_bits != 0x10) return RC522_ERR;
+    return RC522_OK;
+}
+
+rc522_status anticollision(uint8_t **res, uint8_t *res_size, uint8_t *res_size_bits) {
+    uint8_t data[2] = { PICC_ANTICOLL, 0x20 };
+    write_to_register(BitFramingReg, 0x00);
+    rc522_status status = send_command(Transceive, data, 2, res, res_size, res_size_bits, 0);
+
+    if(status != RC522_OK) return status;
+    if(*res_size != 5) return RC522_ERR;
+    if(!is_uid_valid_picc(*res)) return RC522_FAILED_UID_CHECK;
+
+    return RC522_OK;
+}
+
+rc522_status select_tag(uint8_t **res, uint8_t *res_size, uint8_t *res_size_bits, uint8_t *uid) {
+    uint8_t *buffer = kmalloc(9, GFP_KERNEL);
+    buffer[0] = PICC_SElECTTAG;
+    buffer[1] = 0x70;
+    memcpy(buffer+2, uid, 5);
+    
+    calculate_crc(buffer, 7, buffer+7);
+
+    rc522_status status = send_command(Transceive, buffer, 9, res, res_size, res_size_bits, 0);
+    kfree(buffer);
+
+    if(status != RC522_OK) return status;
+    if(*res_size != 3) return RC522_ERR;
+    return RC522_OK;
+}
+
+rc522_status authenticate(uint8_t **res, uint8_t *res_size, uint8_t *res_size_bits, picc_auth_commands auth_mode, uint8_t block_address, uint8_t *sector_key, uint8_t sector_key_size, uint8_t *uid) {
+    uint8_t buffer_size = 2 + sector_key_size + 4;
+    // size = 2 + sector_key_size + 4;1 for the auth_mode and 1 for address, and 4 for the first 4 bytes of uid
+    uint8_t *buffer = kmalloc(buffer_size, GFP_KERNEL);
+    buffer[0] = auth_mode;
+    buffer[1] = block_address;
+    memcpy(buffer+2, sector_key, sector_key_size);
+    memcpy(buffer+2+sector_key_size, uid, 4);
+
+    rc522_status status = send_command(MFAuthent, buffer, buffer_size, res, res_size, res_size_bits, 0);
+    kfree(buffer);
+
+    if(status != RC522_OK) return status;
+    if(!(read_from_register(Status2Reg) & 0x08)) return RC522_ERR;
+    return RC522_OK;
+}
+
+void stop_authentication(void) {
+    clear_bits_in_reg(Status2Reg, 0x08);
+}
+
+rc522_status read_block(uint8_t **res, uint8_t *res_size, uint8_t *res_size_bits, uint8_t blockAddr) {
+    uint8_t *buffer = kmalloc(4, GFP_KERNEL);
+    rc522_status status;
+    buffer[0] = PICC_READ;
+    buffer[1] = blockAddr;
+    status = calculate_crc(buffer, 2, buffer+2);
+    if(status != RC522_OK) return status;
+
+    status = send_command(Transceive, buffer, 4, res, res_size, res_size_bits, 1);
+    kfree(buffer);
+    if(status != RC522_OK) return status;
+    *res_size = (*res_size) - 2;//ignore crc bits
+    //if(*res_size != 16) return RC522_ERR;
+    return RC522_OK;
+}
+
+rc522_status write_block(uint8_t *data, uint8_t data_size, uint8_t **res, uint8_t *res_size, uint8_t *res_size_bits, uint8_t blockAddr) {
+    uint8_t *buffer = kmalloc(4, GFP_KERNEL);
+    rc522_status status;
+    buffer[0] = PICC_WRITE;
+    buffer[1] = blockAddr;
+    status = calculate_crc(buffer, 2, buffer+2);
+    if(status != RC522_OK) return status;
+
+    status = send_command(Transceive, buffer, 4, res, res_size, res_size_bits, 0);
+    kfree(buffer);
+    if(status != RC522_OK) return status;
+    buffer = kmalloc(18, GFP_KERNEL); // 16 bytes of data + 2 of crc
+    kfree(*res);
+    *res_size = 0;
+    *res_size_bits = 0;
+    if(data_size < 16) {
+        memcpy(buffer, data, data_size);
+        for (uint8_t i = data_size; i < 16; i++)
+        {
+            buffer[i] = ' ';
+        }
+    }
+    else {
+        memcpy(buffer, data, 16);
+    }
+    status = calculate_crc(buffer, 16, buffer+16);
+    if(status != RC522_OK) return status;
+
+    status = send_command(Transceive, buffer, 18, res, res_size, res_size_bits, 1);
+    //if(*res_size != 16) return RC522_ERR;
+    return RC522_OK;
+}
+
+/* static int rc522_spi_probe(struct spi_device *spi)
+{
+    printk(KERN_INFO "Probe Chamado\n");
+    rc522_spi_dev = spi;
+
+    uint8_t version = rc522_pcd_setup();
+    printk(KERN_INFO "Version byte 0x%x\n", version);
+
+    rc522_status status = -1;
+    uint8_t *req_res, *uid, *select_tag_res, *auth_res;
+    uint8_t res_size = 0, res_size_bits = 0;
+    uint8_t key[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+    
+    
+    uint16_t i = 300;
+    while(i > 0) {
+        status = req_a_picc(&req_res, &res_size, &res_size_bits);
+        if(status != RC522_OK) {
+            i--;
+            continue;
+        }
+        for (size_t i = 0; i < res_size; i++)
+        {
+            printk(KERN_INFO "req_a_picc[%d] = %x", i, req_res[i]);
+        }
+
+        res_size = 0, res_size_bits = 0;
+        status = anticollision(&uid, &res_size, &res_size_bits);
+        if(status != RC522_OK) {
+            i--;
+            continue;
+        }
+        for (size_t i = 0; i < res_size; i++)
+        {
+            printk(KERN_INFO "anticollision[%d] = %u", i, uid[i]);
+        }
+
+        res_size = 0, res_size_bits = 0;
+        printk(KERN_INFO "-1");
+        status = select_tag(&select_tag_res, &res_size, &res_size_bits, uid);
+        if(status != RC522_OK) {
+            i--;
+            continue;
+        }
+        for (size_t i = 0; i < res_size; i++)
+        {
+            printk(KERN_INFO "select_tag[%d] = %u", i, select_tag_res[i]);
+        }
+
+        res_size = 0, res_size_bits = 0;
+        status = authenticate(&auth_res, &res_size, &res_size_bits, PICC_AUTHENT1A, 11, key, 6, uid);
+        if(status != RC522_OK) {
+            i--;
+            continue;
+        }
+        for (size_t i = 0; i < res_size; i++)
+        {
+            printk(KERN_INFO "auth_res[%d] = %u", i, auth_res[i]);
+        }
+
+        uint8_t buffer[16] = {'N','o','v','a','E','s','c','r','i','t','a','R','C','5','2','2'};
+        uint8_t buffer_size = 16;
+        uint8_t max_data_access = buffer_size / 16;
+        uint8_t complete_data = max_data_access > 3;
+        uint8_t block_addres[3] = {8, 9, 10};
+        max_data_access = complete_data ? 3 : max_data_access;
+        printk(KERN_INFO "write_data");
+        for (uint8_t i = 0; i < max_data_access; i++)
+        {
+            uint8_t *block_data;
+            uint8_t block_res_size = 0, block_res_size_bits = 0;
+            uint8_t *buffer_pointer = buffer + i*16;
+            
+            block_res_size = 0, block_res_size_bits = 0;
+            if(i == max_data_access && !complete_data) {
+                printk(KERN_INFO "write_block 1");
+                status = write_block(buffer_pointer, buffer_size % 16, &block_data, &block_res_size, &block_res_size_bits, block_addres[i]);
+            }
+            else {
+                printk(KERN_INFO "write_block 2");
+                status = write_block(buffer_pointer, 16, &block_data, &block_res_size, &block_res_size_bits, block_addres[i]);
+            }
+
+            if(status != RC522_OK) {
+                printk(KERN_INFO "status not okay");
+                i--;
+                continue;
+            }
+        }
+
+        stop_authentication();
+
+        break;
+    }
+
+    return 0;
+} */
+
 static int rc522_spi_probe(struct spi_device *spi)
 {
     printk(KERN_INFO "Probe Chamado\n");
     rc522_spi_dev = spi;
 
-    //uint8_t version = rc522_pcd_setup();
-    //printk(KERN_INFO "Version byte 0x%x\n", version);
-    rc522_self_test();
+    uint8_t version = rc522_pcd_setup();
+    printk(KERN_INFO "Version byte 0x%x\n", version);
+
+    rc522_status status = -1;
+    uint8_t *req_res, *uid, *select_tag_res, *auth_res;
+    uint8_t res_size = 0, res_size_bits = 0;
+    uint8_t key[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+    
+    
+    uint16_t i = 200;
+    while(i > 0) {
+        status = req_a_picc(&req_res, &res_size, &res_size_bits);
+        if(status != RC522_OK) {
+            i--;
+            continue;
+        }
+        for (size_t i = 0; i < res_size; i++)
+        {
+            printk(KERN_INFO "req_a_picc[%d] = %x", i, req_res[i]);
+        }
+
+        res_size = 0, res_size_bits = 0;
+        status = anticollision(&uid, &res_size, &res_size_bits);
+        if(status != RC522_OK) {
+            i--;
+            continue;
+        }
+        for (size_t i = 0; i < res_size; i++)
+        {
+            printk(KERN_INFO "anticollision[%d] = %u", i, uid[i]);
+        }
+
+        res_size = 0, res_size_bits = 0;
+        printk(KERN_INFO "-1");
+        status = select_tag(&select_tag_res, &res_size, &res_size_bits, uid);
+        if(status != RC522_OK) {
+            i--;
+            continue;
+        }
+        for (size_t i = 0; i < res_size; i++)
+        {
+            printk(KERN_INFO "select_tag[%d] = %u", i, select_tag_res[i]);
+        }
+
+        res_size = 0, res_size_bits = 0;
+        status = authenticate(&auth_res, &res_size, &res_size_bits, PICC_AUTHENT1A, 11, key, 6, uid);
+        if(status != RC522_OK) {
+            i--;
+            continue;
+        }
+        for (size_t i = 0; i < res_size; i++)
+        {
+            printk(KERN_INFO "auth_res[%d] = %u", i, auth_res[i]);
+        }
+
+        uint8_t buffer_size = 0;
+        uint8_t *buffer = kmalloc(16*3, GFP_KERNEL);
+        uint8_t block_addres[3] = {8, 9, 10};
+        printk(KERN_INFO "1");
+        for (uint8_t i = 0; i < 3; i++)
+        {
+            uint8_t *block_data;
+            uint8_t block_res_size = 0, block_res_size_bits = 0;
+            uint8_t *buffer_pointer = buffer + (i * 16);
+
+            block_res_size = 0, block_res_size_bits = 0;
+            status = read_block(&block_data, &block_res_size, &block_res_size_bits, block_addres[i]);
+            if(status != RC522_OK) {
+                i--;
+                continue;
+            }
+            printk(KERN_INFO "2");
+            printk(KERN_INFO "block_data[%u]:",i);
+            for (size_t i = 0; i < block_res_size; i++)
+            {
+                printk(KERN_CONT "%c", block_data[i]);
+            }
+            memcpy(buffer_pointer, block_data, block_res_size);
+            printk(KERN_INFO "block_res_size: %u", block_res_size);
+            buffer_size = buffer_size + block_res_size;
+            printk(KERN_INFO "4");
+        }
+        printk(KERN_INFO "5");
+        printk(KERN_INFO "Read: ");
+        for (size_t i = 0; i < buffer_size; i++)
+        {
+            printk(KERN_CONT "%c", buffer[i]);
+        }
+
+        stop_authentication();
+
+        break;
+    }
 
     return 0;
 }
@@ -211,7 +622,6 @@ static int rc522_spi_probe(struct spi_device *spi)
 static void rc522_spi_remove(struct spi_device *spi)
 {
     // Limpar qualquer recurso alocado
-    rc522_spi_dev = NULL;
 }
 
 static const struct spi_device_id rc522_spi_idtable[] = {
@@ -241,7 +651,7 @@ static struct spi_driver rc522_spi_driver = {
     .id_table = rc522_spi_idtable,
 };
 
-static int __init rc522_spi_init(void)
+int __init rc522_spi_init(void)
 {
     int ret;
     printk(KERN_INFO "Iniciando GPIO Reset\n");
@@ -269,11 +679,15 @@ static int __init rc522_spi_init(void)
     return 0;
 }
 
-static void __exit rc522_spi_exit(void)
+void __exit rc522_spi_exit(void)
 {
+    printk(KERN_INFO "Tirando registo do spi driver\n");
     spi_unregister_driver(&rc522_spi_driver);
+    printk(KERN_INFO "Desativando pino de reset\n");
     gpio_set_value(rc522_rst_pin.gpio, 0);
+    printk(KERN_INFO "Liberando pino de reset\n");
     gpio_free(rc522_rst_pin.gpio);
+    printk(KERN_INFO "Modulo retirado\n");
 }
 
 module_init(rc522_spi_init);
